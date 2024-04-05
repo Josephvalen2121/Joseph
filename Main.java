f29775c2d0babf21ec8efe5a2c1ceba780addf15
Josephvalen2121 <165808404+Josephvalen2121@users.noreply.github.com>
2024-04-05 07:30:49 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+class Main {
+      public static void main( String[] args ) {
+         Movie FastAndFurious= new Movie();
+         FastAndFurious.setYear(2008);
+         System.out.println(" This movie was made in " + FastAndFurious.getYear() + ".");
+     }
+
+  public static class Movie {
+
+    int year = 0;
+
+    public Movie(){}
+
+    public void setYear(int startYear){
+      year = startYear;
+    }
+
+    public int getYear(){
+      return year;
+    }
+  }
+}
